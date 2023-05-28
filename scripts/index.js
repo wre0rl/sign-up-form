7ad412daf5721a4d3a3562f07743009adf927afd
wre0rl <wre0rl@gmail.com>
2023-05-29 05:56:44 +0700

    Password matching validation logic

--- scripts/index.js
+++ scripts/index.js
@@ -10,10 +10,23 @@ const handleInvalidEvent = (event) => {
         input.classList.remove('form__input--state-danger');
         input.nextElementSibling.remove();
       }
+
+      // Password matching validation
+      if (input.id === 'confirm-password') {
+        const password = event.target.querySelector('#password');
+        const confirmPassword = input;
+        if (password.value !== confirmPassword.value) {
+          input.setCustomValidity('Confirm Password doesn\'t match Password');
+          console.log(input);
+        } else {
+          input.setCustomValidity('');
+        }
+      }
+
       // Add new error message
       if (!input.validity.valid) {
         input.classList.add('form__input--state-danger');
-        input.insertAdjacentHTML('afterend', `<p class="error--state-danger">${input.validationMessage}</p>`);
+        input.insertAdjacentHTML('afterend', `<span class="error--state-danger">${input.validationMessage}</span>`);
       }
     });
   }
