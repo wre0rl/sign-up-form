63f474a70c89156686df3aa8a685087087a49581
wre0rl <wre0rl@gmail.com>
2023-05-29 01:03:19 +0700

    Add styling and basic custom javascript error msg

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,15 @@
 const handleInvalidEvent = (event) => {
   event.preventDefault();
-  console.log(event);
+
+  const input = document.querySelector(`#${event.target.id}`);
+  const error = document.createElement('span');
+
+  if (input.classList.contains('form__input--state-danger')) input.nextElementSibling.remove();
+
+  input.classList.add('form__input--state-danger');
+  error.classList.add('class', 'error--state-danger');
+  error.textContent = event.target.validationMessage;
+  input.after(error);
 };
 
 document.addEventListener('invalid', handleInvalidEvent, true);
